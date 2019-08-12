f5fe8fb6a43ebdafc59e52eb7e65c882a7684b6e
Tho Huynh <feeliwood@gmail.com>
2019-08-12 22:05:16 +0700

    Enable swagger

--- src/main/java/ampos/restaurant/RestaurantApplication.java
+++ src/main/java/ampos/restaurant/RestaurantApplication.java
@@ -3,12 +3,14 @@ package ampos.restaurant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * RestaurantApplication is used to initialize application
  *
  */
 @SpringBootApplication
+@EnableSwagger2
 @ComponentScan( { "ampos.restaurant" } )
 public class RestaurantApplication {
     public static void main( String[] args ) {
